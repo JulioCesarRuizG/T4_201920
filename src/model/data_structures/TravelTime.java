a0bd38c6085f465da804588a8b299ca6db02f092
Julio César Ruiz Gómez <jc.ruizg@uniandes.edu.co>
2019-09-23 20:46:01 -0500

    Corrección clase TravelTime

--- src/model/data_structures/TravelTime.java
+++ src/model/data_structures/TravelTime.java
@@ -1,10 +1,24 @@
 package model.data_structures;
 
-public class TravelTime <TravelTime extends Comparable<int>>{
+public class TravelTime implements Comparable<TravelTime>{
+
+	private double meanTravelTime;
+	
+	public double darTravelTime()
+	{
+		return meanTravelTime;
+	}
 
 @Override
-public int compareTo(Object arg0) {
-	// TODO Auto-generated method stub
+public int compareTo(TravelTime t) {
+	if(meanTravelTime > t.darTravelTime())
+	{
+		return 1;
+	}
+	else if(meanTravelTime < t.darTravelTime())
+	{
+		return -1;
+	}
 	return 0;
 }
 }
